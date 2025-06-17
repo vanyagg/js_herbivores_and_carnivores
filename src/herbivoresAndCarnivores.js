a18e226f001d34d4c1242b2feb3456bef0e429ad
Vanya Holovey <golovejvana9@gmail.com>
2025-06-17 21:18:16 +0300

    herbivores and carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(health = 100, name, hidden = false) {
+    this.health = health;
+    this.name = name;
+    this.hidden = hidden;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(100, name);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(100, name);
+  }
+
+  bite(victim) {
+    if (victim instanceof Herbivore && victim.hidden === false) {
+      victim.health -= 50;
+
+      if (victim.health <= 0) {
+        Animal.alive.splice(Animal.alive.indexOf(victim), 1);
+      }
+    }
+  }
 }
 
 module.exports = {
